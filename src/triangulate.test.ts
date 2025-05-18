a9d8a7e1dfd0f7086647cde6d1eb814e34bce6d4
Jeremy Greer <jex.grizzle@gmail.com>
2025-05-18 19:44:29 -0400

    test: add tests for getInfo function to validate timestamp calculations

--- src/triangulate.test.ts
+++ src/triangulate.test.ts
@@ -1,30 +1,96 @@
-describe('triangulate', () => {
-  it('should return coords', () => {
-    type Coords = {
-      x: number
-      y: number
+import { table } from 'console'
+
+type Reference = {
+  ts: number
+}
+
+type TimeBetweenReferences = {
+  a: Reference
+  b: Reference
+  time: number
+}
+
+type Info = {
+  tsA: number
+  tsB?: number
+  deltaAB?: number
+  closest?: Reference
+  ratioDeltaAB?: number
+}
+
+type GetInfoArgs = { a: Reference; b?: Reference; tAB?: TimeBetweenReferences }
+const getInfo = ({ a, b, tAB }: GetInfoArgs): Info => {
+  const deltaAB = b ? b.ts - a.ts : undefined
+  const canGetRatio = tAB && b && deltaAB !== undefined
+  return {
+    tsA: a.ts,
+    tsB: b?.ts,
+    deltaAB,
+    closest: b ? (a.ts < b.ts ? a : b) : undefined,
+    ratioDeltaAB: canGetRatio ? deltaAB / tAB.time : undefined,
+  }
+}
+
+describe('given a single point', () => {
+  it('has no information other than ts', () => {
+    const a: Reference = {
+      ts: 0,
+    }
+    const actual = getInfo({ a })
+    const expected: Info = {
+      tsA: a.ts,
+    }
+    expect(actual).toEqual(expected)
+  })
+})
+
+describe('given two points', () => {
+  it('should have ts for each point', () => {
+    const a: Reference = {
+      ts: 100,
+    }
+    const b: Reference = {
+      ts: 10,
+    }
+    const actual = getInfo({
+      a,
+      b,
+    })
+    const expected: Info = {
+      tsA: a.ts,
+      tsB: b.ts,
+      deltaAB: b.ts - a.ts,
+      closest: b,
+    }
+    expect(actual).toEqual(expected)
+  })
+})
+
+describe('given two points and time (static time between A and B)', () => {
+  it('should have ratio of difference in terms of tAB)', () => {
+    const time = 100
+
+    const a: Reference = {
+      ts: 20,
+    }
+    const b: Reference = {
+      ts: 40,
+    }
+    const tAB: TimeBetweenReferences = {
+      a,
+      b,
+      time,
+    }
+    const actual = getInfo({ a, b, tAB })
+    const expected: Info = {
+      tsA: a.ts,
+      tsB: b.ts,
+      deltaAB: b.ts - a.ts,
+      closest: a,
+      ratioDeltaAB: (b.ts - a.ts) / time,
     }
 
-    type CoordsAndValue = Coords & {
-      value: number
-    }
-
-    const triangulate = (
-      a: CoordsAndValue,
-      b: CoordsAndValue,
-      c: CoordsAndValue,
-    ): Coords => {
-      // return the centroid of the triangle
-      const x = (a.x + b.x + c.x) / 3
-      const y = (a.y + b.y + c.y) / 3
-      return { x, y }
-    }
-
-    const a: CoordsAndValue = { x: 0, y: 0, value: 0 }
-    const b: CoordsAndValue = { x: 3, y: 0, value: 0 }
-    const c: CoordsAndValue = { x: 0, y: 4, value: 0 }
-    const expected: Coords = { x: 1, y: expect.closeTo(1.33) }
-    const coords = triangulate(a, b, c)
-    expect(coords).toEqual(expected)
+    console.log('actual', actual)
+    expect(actual).toEqual(expected)
   })
 })
